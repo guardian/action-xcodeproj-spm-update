8c9d09b40e4436f67b8c4b6a1a2cc07a63e48348
Simone Smith <simone.smith@guardian.co.uk>
2025-11-20 14:32:43 +0000

    don't swallow output

--- entrypoint.sh
+++ entrypoint.sh
@@ -97,7 +97,7 @@ fi
 # If `forceResolution`, then delete the `Package.resolved`
 if [ "$forceResolution" = true ] || [ "$forceResolution" = 'true' ]; then
   echo "Deleting Package.resolved to force it to be regenerated under new format."
-  rm -rf "$RESOLVED_PATH" 2>/dev/null
+  rm -rf "$RESOLVED_PATH"
 fi
 
 # Should be mostly redundant as we use the disable cache flag.
