0cb7f43385c211d214ad1f79a0596d933aa81d43
Simone Smith <simone.smith@guardian.co.uk>
2025-11-25 12:04:09 +0000

    Reinstate new deriveddata logic and delete DD folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -61,31 +61,29 @@ else
 fi
 
 # Default DerivedData path
-#DERIVED_DATA_DIR=~/Library/Developer/Xcode/DerivedData
-#
-#if [ -z "$projectName" ]; then
-#    echo "🔍 Scanning for recent derived data folders..."
-#    echo "Tip: Pass your project/workspace name to narrow results."
-#
-#    # List most recently modified folders
-#    ls -lt "$DERIVED_DATA_DIR" | head -10
-#else
-#    echo "🔍 Looking for DerivedData folder matching: $projectName"
-#
-#    MATCH=$(find "$DERIVED_DATA_DIR" -maxdepth 1 -type d -name "${projectName}-*" | head -n 1)
-#
-#    if [ -n "$MATCH" ]; then
-#        echo "✅ Found DerivedData folder:"
-#        echo "$MATCH"
-#    else
-#        echo "❌ No DerivedData folder found matching: $projectName"
-#    fi
-#fi
+DERIVED_DATA_DIR=~/Library/Developer/Xcode/DerivedData
 
-# Cleanup Caches
-DERIVED_DATA=$(xcodebuild ${xcodebuildInputs} -showBuildSettings -disableAutomaticPackageResolution -skipPackageUpdates | grep -m 1 BUILD_DIR | grep -oE "\/.*" | sed 's|/Build/Products||')
-echo "Found DerivedData folder: $DERIVED_DATA"
-rm -rf "$DERIVED_DATA"
+if [ -z "$projectName" ]; then
+    echo "🔍 Scanning for recent derived data folders..."
+    echo "Tip: Pass your project/workspace name to narrow results."
+
+    # List most recently modified folders
+    ls -lt "$DERIVED_DATA_DIR" | head -10
+else
+    echo "🔍 Looking for DerivedData folder matching: $projectName"
+
+    MATCH=$(find "$DERIVED_DATA_DIR" -maxdepth 1 -type d -name "${projectName}-*" | head -n 1)
+
+    if [ -n "$MATCH" ]; then
+        echo "✅ Found DerivedData folder:"
+        echo "$MATCH"
+        echo "Deleting DerivedData folder"
+        rm -rf $MATCH
+
+    else
+        echo "❌ No DerivedData folder found matching: $projectName"
+    fi
+fi
 
 # If `forceResolution`, then delete the `Package.resolved`
 if [ "$forceResolution" = true ] || [ "$forceResolution" = 'true' ]; then
